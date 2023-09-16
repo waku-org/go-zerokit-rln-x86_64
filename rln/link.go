2a77c3734dd190404c072d6378140045aa7aafe5
Richard Ramos <info@richardramos.me>
2023-09-16 13:03:46 -0400

    chore: add reference to library folders for go mod vendor

--- rln/link.go
+++ rln/link.go
@@ -1,5 +1,11 @@
 package rln
 
+import (
+	_ "github.com/waku-org/go-zerokit-rln-x86_64/libs/x86_64-pc-windows-gnu"
+	_ "github.com/waku-org/go-zerokit-rln-x86_64/libs/x86_64-unknown-linux-gnu"
+	_ "github.com/waku-org/go-zerokit-rln-x86_64/libs/x86_64-unknown-linux-musl"
+)
+
 /*
 #cgo LDFLAGS: -lrln -ldl -lm
 #cgo linux,amd64,musl,!android LDFLAGS:-L${SRCDIR}/../libs/x86_64-unknown-linux-musl
